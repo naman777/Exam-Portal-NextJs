1defa4ab32de8a34bdbc9bd61708e659f4102622
Naman Kundra <namankundra6@gmail.com>
2024-10-29 02:52:53 +0530

    next-auth local url fix

--- exam-portal-app/lib/authOptions.ts
+++ exam-portal-app/lib/authOptions.ts
@@ -2,11 +2,17 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-    providers:[
+    providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            // Set the authorization URL to include the callback URL
+            authorization: {
+                params: {
+                    redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
+                },
+            },
+        }),
     ],
     secret: process.env.SECRET as string,
-}
+};
